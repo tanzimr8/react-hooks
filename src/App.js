3fbcb4893063416adbe7e2c162c17f7c3369ac5b
Tanzim <tanzim008@gmail.com>
2023-08-12 20:41:15 -0230

    using the useState hook

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
 import { useState } from "react"; 
- 
+ import './App.css';
 export default function App() { 
-  const [greeting, setGreeting] = useState({ greet: "Hello, World" }); 
-  console.log(greeting, setGreeting); 
- 
-  function updateGreeting() {  
-    setGreeting((prevState)=>{
-      return {...prevState, greet: "Hi Workd"}
-    }); 
-  } 
- 
-  return ( 
-    <div> 
-      <h1>{greeting.greet}</h1> 
-      <button onClick={updateGreeting}>Update greeting</button> 
-    </div> 
-  ); 
+  const [formData, setFormData] = useState({
+    email: '',
+    number:''
+  });
+  const handleChange = (e)=>{
+    setFormData({...formData, [e.target.name]: e.target.value});
+  }
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+    if(valiDateForm()){
+      resetForm();
+      alert('Form submitted! Thanks');
+    }
+    
+  }
+  const valiDateForm = ()=>{
+    return (formData.email && formData.number.length >2);
+  }
+  const resetForm = ()=>{
+    setFormData({email: '',number:''})
+  }
+  return(
+    <form onSubmit={handleSubmit}>
+      <input type="email" name="email" value={formData.email} onChange={handleChange} placeholder="Your email" className={!formData.email ? 'redBorder' : 'form-border'}/>
+      <input type="text" name="number" value={formData.number} onChange={handleChange} placeholder="Your Phone number" className={!formData.email ? 'redBorder' : 'form-border'}/>
+      <button type="submit">Submit</button>
+    </form>
+  );
 } 
